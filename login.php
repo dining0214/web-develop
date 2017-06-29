0b0173aaa5a541f5e44ea2e574982e766e2ad132
dining0214 <dining0214@gmail.com>
2017-06-30 07:27:35 +0800

    different type of outlook

--- login.php
+++ login.php
@@ -1,21 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-  <!-- bootstrap CDN -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-  <noscript>
-    <link rel="stylesheet" href="css/style.css" />
-  </noscript>
-  <link rel="stylesheet" href="main.css">
-
-	<title>使用者登入系統</title>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <link rel="stylesheet" href="login.css">
 </head>
-<body class="loginPage" style="background-color:white;">
-  <!-- 會員登入程序 -->
+<body>
+    <!-- 會員登入程序 -->
   <?php  
     $msg="";
     session_start();
@@ -34,55 +25,36 @@
           $_SESSION["login_session"]=true;
           $_SESSION["User"]=$username;
           header("location:index.php");
-          $msg = "登入成功";
+          $msg = "Login suceesfully.";
         }
         else{
-          $msg = "使用者名稱或密碼錯誤！";
+          $msg = "The username or password is incorrect！";
         }
 
     }
   ?>
-
-  
-	<header id='header'>
-      <div class='logo'>
-        <img src='http://archive.download.redhat.com/pub/redhat/linux/7.0/en/os/alpha/RedHat/instimage/usr/share/anaconda/pixmaps/first.png' height="65" width="65" class="imgg" />
-      </div>
-
-      <div class='center'>
-        <h3>
-         	<a href="#">Welcome to GROWGORO!</a>
-        </h3>	
-      </div>
-      
-    </header>
-
-		</div>
-  <!-- 登入表單 -->
-  <div class="jumbotron">
-    <h1 class="Title text-center" style="font-family:Microsoft JhengHei">登入系統</h1>
-    <form class="form-horizontal center" role="form"  method="post" action="">
-        <div class="form-group">
-          <label  class="col-sm-4 control-label" style="font-family:Microsoft JhengHei">使用者名稱</label>
-          <div class="col-sm-4">
-              <input type="text" class="form-control" style="font-family:Microsoft JhengHei" id="Username" name="Username" placeholder="請輸入使用者名稱">
-          </div>
-        </div>
-        <div class="form-group">
-          <label class="col-sm-4 control-label" style="font-family:Microsoft JhengHei">密碼</label>
-          <div class="col-sm-4">
-              <input type="password" class="form-control" style="font-family:Microsoft JhengHei" id="Password" name="Password" placeholder="密碼">
-          </div>
-        </div>
-        <div class="form-group">
-          <div class="col-sm-offset-4 col-sm-10">
-              <button type="submit" class="btn btn-success" style="font-family:Microsoft JhengHei">登入</button>
-              <input type="button" class="btn btn-warning" style="font-family:Microsoft JhengHei" onclick="location.href='register.php'" value="註冊">
-              <br>
-              <?php echo $msg; ?>
-          </div>
-        </div>
-    </form>
+  <div class="body"></div>
+  <div class="grad"></div>
+  <div class="header">
+    <div>Grow<span>goro</span></div>
   </div>
+  <br>
+  <form role="form" method="post" action="">
+      <div class="login">
+          <input type="text" placeholder="username" id="Username" name="Username" ><br>
+          <input type="password" placeholder="password" id="Password" name="Password" ><br>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+          <div>
+            <input type="button" onclick="location.href='register.php'" value="Register">
+          </div>
+          <br>
+          <div class="showmsg">
+            <?php echo $msg ?>  
+          </div>
+      </div>
+  </form>
+
 </body>
 </html>
